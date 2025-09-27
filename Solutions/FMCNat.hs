f851e2b3a6a331d011ee048e9363a6b6e203f53b
Júlia de Medeiros Borba <borba.julia06@gmail.com>
2025-09-27 07:58:00 -0300

    fmcnat

--- Solutions/FMCNat.hs
+++ Solutions/FMCNat.hs
@@ -19,7 +19,7 @@ import Prelude
     )
 import Distribution.Simple.Setup (trueArg, TestFlags (testDistPref))
 import Text.XHtml (tt)
-import System.Win32 (xBUTTON1)
+import System.Win32 (xBUTTON1, SECURITY_ATTRIBUTES (nLength))
 
 -- Define evenerything that is undefined,
 -- without using standard Haskell functions.
@@ -69,6 +69,20 @@ instance Ord Nat where
     max x O = x
     max (S x) (S y) = max x y
 
+----------------------------------------------------------------
+-- some sugar
+----------------------------------------------------------------
+
+zero, one, two, three, four, five, six, seven, eight :: Nat
+zero  = O
+one   = S zero
+two   = S one
+three = S two
+four  = S three
+five  = S four
+six   = S five
+seven = S six
+eight = S seven
 
 ----------------------------------------------------------------
 -- internalized predicates
@@ -100,14 +114,17 @@ odd (S (S x)) = odd x
 
 -- addition
 (<+>) :: Nat -> Nat -> Nat
-(<+>) = undefined
+(<+>) x O = x
+(<+>) x (S y) = S (x + y)
 
 -- This is called the dotminus or monus operator
 -- (also: proper subtraction, arithmetic subtraction, ...).
 -- It behaves like subtraction, except that it returns 0
 -- when "normal" subtraction would return a negative number.
 monus :: Nat -> Nat -> Nat
-monus = undefined
+monus x O = x
+monus O x = O
+monus (S x) (S y) = monus x y
 
 (-*) :: Nat -> Nat -> Nat
 (-*) = monus
@@ -117,36 +134,66 @@ monus = undefined
 
 -- multiplication
 times :: Nat -> Nat -> Nat
-times = undefined
+(times) x  O = O
+(times) x (S O) = x
+(times) x (S y) = (<+>) (times x y) x
 
 (<*>) :: Nat -> Nat -> Nat
 (<*>) = times
 
 -- power / exponentiation
 pow :: Nat -> Nat -> Nat
-pow = undefined
+(pow) x O = S O
+(pow) (S O) x = S O
+(pow) x (S y) = pow x y * x
 
 exp :: Nat -> Nat -> Nat
-exp = undefined
+exp = pow
 
 (<^>) :: Nat -> Nat -> Nat
-(<^>) = undefined
+(<^>) = pow
 
 -- quotient
 (</>) :: Nat -> Nat -> Nat
-(</>) = undefined
+(</>) O x = O
+(</>) x y =
+  case y of
+    O -> undefined
+    S z ->
+      case monus x y of
+        O ->
+          case monus y x of
+            O -> one
+            w -> O
+        S w -> S ((</>) (x -* y) y)
 
 -- remainder
 (<%>) :: Nat -> Nat -> Nat
-(<%>) = undefined
+(<%>) O x = O
+(<%>) x y =
+  case y of
+    O -> undefined
+    S z ->
+      case monus x y of
+        O ->
+          case monus y x of
+            O -> O
+            w -> x
+        S w -> (<%>) (x -* y) y
 
 -- euclidean division
 eucdiv :: (Nat, Nat) -> (Nat, Nat)
-eucdiv = undefined
+eucdiv (x, y) = ((</>) x y, (<%>) x y)
 
 -- divides
 (<|>) :: Nat -> Nat -> Bool
-(<|>) = undefined
+(<|>) y x =
+  case y of
+    O -> undefined
+    z ->
+      case (<%>) x y of
+        O -> True
+        w -> False
 
 divides = (<|>)
 
@@ -155,20 +202,36 @@ divides = (<|>)
 -- x `dist` y = |x - y|
 -- (Careful here: this - is the real minus operator!)
 dist :: Nat -> Nat -> Nat
-dist = undefined
+dist O O = O
+dist O x = x
+dist x O = x
+dist (S x) (S y) = dist x y
 
 (|-|) = dist
 
 factorial :: Nat -> Nat
-factorial = undefined
+factorial O = one
+factorial (S O) = one
+factorial (S x) = S x * factorial x
 
 -- signum of a number (-1, 0, or 1)
 sg :: Nat -> Nat
-sg = undefined
+sg O = zero
+sg x = S O
 
 -- lo b a is the floor of the logarithm base b of a
 lo :: Nat -> Nat -> Nat
-lo = undefined
+lo j u =
+  case j of
+    O -> undefined
+    (S O) -> undefined
+    l ->
+      case monus u j of
+        O ->
+          case monus j u of
+            O -> one
+            S i -> O
+        S a -> S (lo j ((</>) u j))
 
 
 ----------------------------------------------------------------
@@ -179,11 +242,15 @@ lo = undefined
 -- Do NOT use the following functions in the definitions above!
 
 toNat :: Integral a => a -> Nat
-toNat = undefined
+toNat 0 = O
+toNat i =
+    if i <= 0
+        then undefined
+        else S (toNat (i - 1))
 
 fromNat :: Integral a => Nat -> a
-fromNat = undefined
-
+fromNat O = 0
+fromNat (S n) = 1 + fromNat n
 
 -- Voilá: we can now easily make Nat an instance of Num.
 instance Num Nat where
@@ -195,6 +262,6 @@ instance Num Nat where
     signum = sg
     fromInteger x
       | x < 0     = undefined
-      | x == 0    = undefined
-      | otherwise = undefined
+      | x == 0    = O
+      | otherwise = S (fromInteger (x - 1))
 
