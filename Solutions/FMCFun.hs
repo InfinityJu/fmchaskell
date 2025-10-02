a484888e2f89122e81942875b671d8f1e33338c3
Júlia de Medeiros Borba <borba.julia06@gmail.com>
2025-10-02 19:07:14 -0300

    fmc fun + correção de arquivos

--- Solutions/FMCFun.hs
+++ Solutions/FMCFun.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE GADTs #-}
 
-module ExList where
+module FMCFun where
 
 import Prelude hiding
     ( (.) , ($)
@@ -12,28 +12,35 @@ import Prelude hiding
 
 -- curry takes a "traditional" binary function
 -- and returns its currified version
-curry :: undefined
-curry = undefined
+curry :: ((x, y) -> z) -> (x -> y -> z)
+curry f x y = f(x, y)
 
 -- uncurry takes a currified function
 -- and returns its "traditional" binary version
-uncurry :: undefined
-uncurry = undefined
+uncurry :: (x -> y -> z) -> ((x, y) -> z)
+uncurry f(x, y) = f x y
 
 -- flip takes a (currified) binary function
 -- and returns one that behaves the same but takes its arguments in the opposite order
+flip :: (x -> y -> z) -> (y -> x -> z)
+flip f x y = f y x
 
 -- (.) takes two composable functions and returns their composition
+(.) :: (y -> z) -> (x -> y) -> (x -> z)
+(.) f g x = f (g x)
 
 -- (.>) is composition but in diagramatic notation (should be ; but Haskell forbids)
+(.>) :: (x -> y) -> (y -> z) -> x -> z
 (.>) = flip (.)
 
 -- ($) takes a function and a suitable argument and applies the function to the argument
 -- think: why would we ever want that?
+($) :: (x -> y) -> x -> y
+f $ x = f x
 
 -- iterate: figure it out by its type
 iterate :: (a -> a) -> a -> [a]
-iterate = undefined
+iterate f a = a : iterate f(f a)
 
 -- orbit
 orbit = flip iterate
